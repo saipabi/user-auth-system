970b2fec9565fb0fdd4172ffa71c03eb96f860c6
Suriya <saipabi123@gmailcom>
2026-01-19 19:50:21 +0530

    Initial commit (remove hardcoded secrets)

---
+++ config/.env.php
@@ -0,0 +1,14 @@
+# MySQL Details
+DB_HOST=mysql-233abac6-saipabi123-f4e0.j.aivencloud.com
+DB_PORT=18809
+DB_USER=avnadmin
+DB_PASS=AVNS_uwQSL1oBq0Gt_ZS47Zc
+DB_NAME=defaultdb
+
+# MongoDB Details
+MONGO_URI=mongodb+srv://auth:OK69bKOBRrcIOlIv@cluster0.zvqghry.mongodb.net/?appName=Cluster0
+MONGO_DB_NAME=internship_task
+
+# Redis Details
+REDIS_HOST=127.0.0.1
+REDIS_PORT=6379
--- config/mysql.php
+++ config/mysql.php
@@ -1,18 +1,18 @@
 <?php
-// mysql.php
+require_once __DIR__ . '/env.php';
+
 $conn = mysqli_init();
-mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL); // MySQL strictly need this
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
 
 $success = mysqli_real_connect(
-    $conn, 
-    "mysql-233abac6-saipabi123-f4e0.j.aivencloud.com", // Host
-    "avnadmin", // User
-    "AVNS_uwQSL1oBq0Gt_ZS47Zc", // Password
-    "defaultdb", // Database Name
-    18809 // Port
+    $conn,
+    getenv('DB_HOST'),
+    getenv('DB_USER'),
+    getenv('DB_PASS'),
+    getenv('DB_NAME'),
+    (int) getenv('DB_PORT')
 );
 
 if (!$success) {
     die("Connection failed: " . mysqli_connect_error());
 }
-?>
